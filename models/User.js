c5204e4bdd75d5d035a1fb41920d5d959c82fb2f
cywind1 <chingjusttest@gmail.com>
2023-03-13 10:21:36 +0100

    3.2_MongoDB_User_Note

--- models/User.js
+++ models/User.js
@@ -1,35 +1,24 @@
 const mongoose = require("mongoose");
-const AutoIncrement = require("mongoose-sequence")(mongoose);
 
-const noteSchema = new mongoose.Schema(
-  {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: "User",
-    },
-    title: {
-      type: String,
-      required: true,
-    },
-    text: {
-      type: String,
-      required: true,
-    },
-    completed: {
-      type: Boolean,
-      default: false,
-    },
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
   },
-  {
-    timestamps: true,
-  }
-);
-
-noteSchema.plugin(AutoIncrement, {
-  inc_field: "ticket",
-  id: "ticketNums",
-  start_seq: 500,
+  password: {
+    type: String,
+    required: true,
+  },
+  roles: [
+    {
+      type: String,
+      default: "Employee",
+    },
+  ],
+  active: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-module.exports = mongoose.model("Note", noteSchema);
+module.exports = mongoose.model("User", userSchema);
